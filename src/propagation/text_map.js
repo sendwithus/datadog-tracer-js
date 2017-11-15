e8dc6de3bf0483e7398ad13f3d81a21723a9fc1a
Daniel Steuernol <daniel@sendwithus.com>
2017-11-15 13:07:03 -0800

    convert id's to base 16

--- src/propagation/text_map.js
+++ src/propagation/text_map.js
@@ -1,43 +1,46 @@
-'use strict'
+"use strict";
 
-const Long = require('long')
-const DatadogSpanContext = require('../span_context')
+const Long = require("long");
+const DatadogSpanContext = require("../span_context");
 
 class TextMapPropagator {
-  inject (spanContext, carrier) {
+  inject(spanContext, carrier) {
     Object.assign(carrier, {
-      'ot-tracer-traceid': spanContext.traceId.toString(),
-      'ot-tracer-spanid': spanContext.spanId.toString(),
-      'ot-tracer-sampled': String(spanContext.sampled)
-    })
+      "ot-tracer-traceid": spanContext.traceId.toString(16),
+      "ot-tracer-spanid": spanContext.spanId.toString(16),
+      "ot-tracer-sampled": String(spanContext.sampled)
+    });
 
-    spanContext.baggageItems && Object.keys(spanContext.baggageItems).forEach(key => {
-      carrier[`ot-baggage-${key}`] = JSON.stringify(spanContext.baggageItems[key])
-    })
+    spanContext.baggageItems &&
+      Object.keys(spanContext.baggageItems).forEach(key => {
+        carrier[`ot-baggage-${key}`] = JSON.stringify(
+          spanContext.baggageItems[key]
+        );
+      });
   }
 
-  extract (carrier) {
-    const baggageItems = {}
+  extract(carrier) {
+    const baggageItems = {};
 
     try {
       Object.keys(carrier).forEach(key => {
-        const match = key.match(/^ot-baggage-(.+)$/)
+        const match = key.match(/^ot-baggage-(.+)$/);
 
         if (match) {
-          baggageItems[match[1]] = JSON.parse(carrier[key])
+          baggageItems[match[1]] = JSON.parse(carrier[key]);
         }
-      })
+      });
 
       return new DatadogSpanContext({
-        traceId: Long.fromString(carrier['ot-tracer-traceid'], true),
-        spanId: Long.fromString(carrier['ot-tracer-spanid'], true),
-        sampled: JSON.parse(carrier['ot-tracer-sampled']),
+        traceId: Long.fromString(carrier["ot-tracer-traceid"], true),
+        spanId: Long.fromString(carrier["ot-tracer-spanid"], true),
+        sampled: JSON.parse(carrier["ot-tracer-sampled"]),
         baggageItems
-      })
+      });
     } catch (e) {
-      return null
+      return null;
     }
   }
 }
 
-module.exports = TextMapPropagator
+module.exports = TextMapPropagator;
